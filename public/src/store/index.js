6db318ff836cb6b920fb150fa197a7f15c0a4cc0
Zweer <niccolo@olivieriachille.com>
2018-04-24 12:11:37 +0200

    Added local storage persistence

--- public/src/store/index.js
+++ public/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import * as login from './modules/login';
 
@@ -8,4 +9,5 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: { login },
   strict: IS_PROD,
+  plugins: [createPersistedState()],
 });
