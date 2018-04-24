13c48714a809e34845a739ff42a5d26e670d1780
Zweer <niccolo@olivieriachille.com>
2018-04-24 12:18:31 +0200

    Moved the social login to its own component

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,47 +1,25 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <fb-signin-button
-      v-if="!isUser"
-      :params="fbSignInParams"
-      @success="onSignInSuccess"
-      @error="onSignInError">
-      Sign in with Facebook
-    </fb-signin-button>
+
+    <SocialLogin v-if="!isUser"/>
 
     <router-view v-else/>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters } from 'vuex';
+
+import SocialLogin from './components/SocialLogin.vue';
 
 export default {
   name: 'App',
 
+  components: { SocialLogin },
+
   computed: {
     ...mapGetters(['isUser']),
-  },
-
-  data() {
-    return {
-      fbSignInParams: {
-        scope: 'public_profile,email',
-        return_scopes: true,
-      },
-    };
-  },
-
-  methods: {
-    onSignInSuccess({ authResponse }) {
-      this.setFacebook(authResponse);
-    },
-
-    onSignInError(error) {
-      console.log('OH NOES', error);
-    },
-
-    ...mapActions(['setFacebook']),
   },
 };
 </script>
@@ -54,14 +32,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.fb-signin-button {
-  display: inline-block;
-  padding: 4px 8px;
-  border-radius: 3px;
-  background-color: #4267b2;
-  color: #fff;
-  cursor: pointer;
 }
 </style>
---
+++ public/src/components/SocialLogin.vue
@@ -0,0 +1,48 @@
+<template>
+  <fb-signin-button
+    :params="fbSignInParams"
+    @success="onSignInSuccess"
+    @error="onSignInError">
+    Sign in with Facebook
+  </fb-signin-button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      fbSignInParams: {
+        scope: 'public_profile,email',
+        return_scopes: true,
+      },
+    };
+  },
+
+  methods: {
+    onSignInSuccess({ authResponse }) {
+      this.setFacebook(authResponse);
+    },
+
+    onSignInError(error) {
+      console.log('OH NOES', error);
+    },
+
+    ...mapActions(['setFacebook']),
+  },
+};
+</script>
+
+<style>
+.fb-signin-button {
+  display: inline-block;
+  padding: 4px 8px;
+  border-radius: 3px;
+  background-color: #4267b2;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
