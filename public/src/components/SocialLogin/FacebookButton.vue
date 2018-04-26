705b57dfa7668347b24edd339feb9d941b26a4ca
Zweer <niccolo@olivieriachille.com>
2018-04-26 09:45:14 +0200

    Removed the example component

--- public/src/components/SocialLogin/FacebookButton.vue
+++ public/src/components/SocialLogin/FacebookButton.vue
@@ -11,7 +11,7 @@
 import { mapActions } from 'vuex';
 
 export default {
-  name: 'App',
+  name: 'FacebookButton',
 
   data() {
     return {
@@ -36,7 +36,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
   .fb-signin-button {
     display: inline-block;
     padding: 4px 8px;
