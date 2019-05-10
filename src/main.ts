b5a754111a9119cc6d5d8ae719483a30110cce78
Zweer <n.olivieriachille@gmail.com>
2019-05-10 12:48:04 +0200

    Tidied up the "app" files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+import { config } from 'node-config-ts';
+
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(config.server.port);
 }
+
 bootstrap();
