8025c72f14261e6ec4309e1cac579f703d8a7429
Zweer <n.olivieriachille@gmail.com>
2019-05-14 11:30:44 +0200

    Added more documentation

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Delete, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiOkResponse, ApiUseTags } from '@nestjs/swagger';
+import {
+  ApiImplicitFile,
+  ApiImplicitParam,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUseTags,
+} from '@nestjs/swagger';
 
 import { Question } from './entities/question.entity';
 
@@ -11,24 +17,31 @@ export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
   @Get('/')
-  @ApiOkResponse({ description: 'Retrieves the complete list of questions (with their answers)', isArray: true, type: Question })
+  @ApiOperation({ title: 'Questions list', description: 'Retrieves the complete list of questions (with their answers)'})
+  @ApiOkResponse({ description: 'All the questions (with answers)', isArray: true, type: Question })
   async list(): Promise<Question[]> {
     return this.questionsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ title: 'Retrieves a question', description: 'Retrieves a single question (with its answers)' })
+  @ApiImplicitParam({ name: 'id', description: 'The id of the question you want to retrieve', required: true, type: 'string' })
   @ApiOkResponse({ description: 'Retrieves the single question (with its answers) having the specified "id"', type: Question })
   async retrieve(@Param('id') id: string): Promise<Question> {
     return this.questionsService.findOne(id);
   }
 
   @Delete(':id')
+  @ApiOperation({ title: 'Deletes a question', description: 'Deletes an answer' })
+  @ApiImplicitParam({ name: 'id', description: 'The id of the question you want to delete', required: true, type: 'string' })
   async destroy(@Param('id') id: string): Promise<void> {
     return this.questionsService.destroy(id);
   }
 
   @Post('import')
   @UseInterceptors(FileInterceptor('questions'))
+  @ApiOperation({ title: 'Imports new questions and answers', description: 'Via a single CSV file it imports questions' })
+  @ApiImplicitFile({ name: 'questions', required: true, description: 'CSV containing the list of questions and answers' })
   @ApiOkResponse({ description: 'Inserts the questions specified in the CSV and returns them', isArray: true, type: Question })
   async import(@UploadedFile() file): Promise<Question[]> {
     const csv = file.buffer.toString();
